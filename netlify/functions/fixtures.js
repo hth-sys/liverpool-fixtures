f70854402cd03c338675442a90664e936647b1e8
hth-sys <hth@ancora.is>
2026-04-08 13:54:41 +0000

    Update fixtures.js

--- netlify/functions/fixtures.js
+++ netlify/functions/fixtures.js
@@ -1,6 +1,6 @@
 exports.handler = async function() {
   const API_KEY = '792e4e6cd1eab9103b6d748131372325';
-  const url = 'https://v3.football.api-sports.io/fixtures?team=40&next=20';
+  const url = 'https://v3.football.api-sports.io/fixtures?team=40&season=2024';
 
   try {
     const res = await fetch(url, {
